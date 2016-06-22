e3191846bc181c7480557de2d9219fc0878443e9
Rafael Ortega Bueno <rafael@uecommerce.com.br>
2016-06-22 14:10:17 -0300

    Create language pack base

---
+++ registration.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Magento2 Portuguese Language Pack (pt_BR)
+ *
+ * Bitbucket: https://bitbucket.org/uecommerce/magento2-language-pt-br
+ * Gihub: https://github.com/rorteg/magento2-language-pt-br
+ *
+ * @author Uecommerce Team <contato@uecommerce.com.br>
+ * @copyright Copyright © 2016 Uecommerce
+ * @version 100.0.5
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
+    'uecommerce_pt_br',
+    __DIR__
+);
+?>
